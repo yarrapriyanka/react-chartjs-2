1bc5881131c8517c6739fa44cdaed546be943c32
yarrapriyanka <priyanka@nebulaesoft.com>
2017-10-18 20:29:00 +0530

    Update line.js

--- example/src/components/line.js
+++ example/src/components/line.js
@@ -8,6 +8,7 @@ const data = {
       label: 'My First dataset',
       fill: false,
       lineTension: 0.1,
+      boxShadow:'0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)',
       backgroundColor: 'rgba(75,192,192,0.4)',
       borderColor: 'rgba(75,192,192,1)',
       borderCapStyle: 'butt',
